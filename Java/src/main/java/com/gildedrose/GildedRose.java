d37ccfa8d8af84d0ef5a5f4dbc1097854364f5c0
DamnMusz <damianfiuba@gmail.com>
2023-02-23 16:47:48 -0300

    First refactor: split into methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     static final String AGED_BRIE = "Aged Brie";
     static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
@@ -15,56 +17,66 @@ class GildedRose {
         return items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(GildedRose.AGED_BRIE)
-                    && !items[i].name.equals(GildedRose.BACKSTAGE_PASS)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(GildedRose.SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(GildedRose.BACKSTAGE_PASS)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+    public void updateQuality(Item item) {
+        if (!item.name.equals(GildedRose.AGED_BRIE)
+            && !item.name.equals(GildedRose.BACKSTAGE_PASS)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(GildedRose.SULFURAS)) {
+                    item.quality = item.quality - 1;
                 }
             }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-            if (!items[i].name.equals(GildedRose.SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(GildedRose.AGED_BRIE)) {
-                    if (!items[i].name.equals(GildedRose.BACKSTAGE_PASS)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(GildedRose.SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                if (item.name.equals(GildedRose.BACKSTAGE_PASS)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-                    } else {
-                        items[i].quality = 0;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
         }
+    }
+
+    public void updateSellInDate(Item item) {
+        if (!item.name.equals(GildedRose.SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    public void updateExpired(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.name.equals(GildedRose.AGED_BRIE)) {
+                if (!item.name.equals(GildedRose.BACKSTAGE_PASS)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(GildedRose.SULFURAS)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
+
+    public void updateQuality() {
+        Arrays.stream(items).forEach(item -> {
+            updateQuality(item);
+            updateSellInDate(item);
+            updateExpired(item);
+        });
     }
 }
